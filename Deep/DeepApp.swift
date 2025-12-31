6d89ebbd95fc1e6b54666b4f708314b220718998
aadishiv <aadishiv@outlook.com>
2025-12-31 17:51:50 -0500

    Initial Commit

---
+++ Deep/DeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepApp.swift
+//  Deep
+//
+//  Created by Aadi Shiv Malhotra on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct DeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeepTests/DeepTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeepTests.swift
+//  DeepTests
+//
+//  Created by Aadi Shiv Malhotra on 12/31/25.
+//
+
+import Testing
+@testable import Deep
+
+struct DeepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
